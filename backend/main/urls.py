b6e0fd3040103b37c0b8f2fdde3e38ab5ded6dae
DaoKhanhNinh-B23DCCN632 <ninhdao2005@gmail.com>
2026-02-25 16:56:00 +0700

    URL Routing

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
-    path('dashboard/', views.dashboard, name='dashboard') 
+    path('dashboard/', views.dashboard, name='dashboard'), 
+    path('change_password/', views.change_password, name='change_password'), 
+    path('create_project/', views.create_project, name='create_project'), 
+    path('create_task/', views.create_task, name='create_task'), 
+    path('project/', views.project, name='project'), 
+    path('task/', views.task, name='task'), 
+    path('update_per_info/', views.update_per_info, name='update_per_info') 
 ]
--- backend/main/views.py
+++ backend/main/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse  
 
-# Create your views here.
 
 def login(request): 
     return render(request, 'main/login.html')
-
 def dashboard(request): 
     return render(request, 'main/dashboard.html') 
+def change_password(request): 
+    return render(request, 'main/change_password.html') 
+def create_project(request): 
+    return render(request, 'main/create_project.html') 
+def create_task(request): 
+    return render(request, 'main/create_task.html')
+def project(request): 
+    return render(request, 'main/project.html')
+def task(request): 
+    return render(request, 'main/task.html')
+def update_per_info(request): 
+    return render(request, 'main/update_per_info.html')
 
 
