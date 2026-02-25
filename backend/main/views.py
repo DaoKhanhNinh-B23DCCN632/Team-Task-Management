649e6e577ce9a5215b0e637075c53ac3e9778e86
DaoKhanhNinh-B23DCCN632 <ninhdao2005@gmail.com>
2026-02-25 16:23:57 +0700

    backend

---
+++ backend/main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse  
+
+# Create your views here.
+
+def login(request): 
+    return render(request, 'main/login.html')
+
+def dashboard(request): 
+    return render(request, 'main/dashboard.html') 
+
+
