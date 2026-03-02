0523c9a89a99657076704b623c82afac46074e8d
DaoKhanhNinh-B23DCCN632 <ninhdao2005@gmail.com>
2026-03-02 16:56:29 +0700

    reset password

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path 
 from . import views 
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', views.login, name='login'),
@@ -9,5 +11,9 @@ urlpatterns = [
     path('create_task/', views.create_task, name='create_task'), 
     path('project/', views.project, name='project'), 
     path('task/', views.task, name='task'), 
-    path('update_per_info/', views.update_per_info, name='update_per_info') 
+    path('update_per_info/', views.update_per_info, name='update_per_info'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='main/password_reset.html'), name='password_reset'), 
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'), 
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'), 
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
 ]
